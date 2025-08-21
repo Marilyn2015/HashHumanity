34cc7a37c7cf1ec50b56a041c4b9370a0d083ac9
Marilyn Stephens <phoenixbird83@hotmail.com>
2025-08-21 06:41:50 -0400

    Update firebaseConfig.js

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,1 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBT7P7DAfV-I9ESe6f9Jdp5ioCyGIK0d9A",
+  authDomain: "hashhumanity-58b9a.firebaseapp.com",
+  databaseURL: "https://hashhumanity-58b9a-default-rtdb.firebaseio.com",
+  projectId: "hashhumanity-58b9a",
+  storageBucket: "hashhumanity-58b9a.appspot.com", // ✅ FIXED HERE
+  messagingSenderId: "886745899016",
+  appId: "1:886745899016:web:2b0f7e043c2434379d71bd",
+  measurementId: "G-3ZTCXL4374"
+};
 
